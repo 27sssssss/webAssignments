a8f6c7b70e1ab8965581650c6f44d1e625cef09e
27ssss <vinniksemen4@gmail.com>
2025-02-17 20:20:56 -0500

    123

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ db.initialize(process.env.MONGODB_CONN_STRING)
       }
     });
 
-    const HTTP_PORT = process.env.PORT || 8080; 
+    const HTTP_PORT = process.env.PORT || 3000; 
     app.listen(HTTP_PORT, () =>
       console.log(`server listening on: ${HTTP_PORT}`)
     );
